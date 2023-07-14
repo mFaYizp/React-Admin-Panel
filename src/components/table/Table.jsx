ef11c0093beca8b50c2ca91c6bdf2ba66602c463
Mohammed Fayiz <fayizfaz78@gmail.com>
2023-07-14 22:39:49 +0530

    user page

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -79,17 +79,19 @@ const List = () => {
           {rows.map((row) => (
             <TableRow key={row.id}>
               <TableCell>{row.id}</TableCell>
-              <TableCell className="tableCell" >
-              <div className="cellWrapper">
-                <img src={row.img} alt="" className="image" />
-                {row.product}
-              </div>
+              <TableCell className="tableCell">
+                <div className="cellWrapper">
+                  <img src={row.img} alt="" className="image" />
+                  {row.product}
+                </div>
               </TableCell>
-              <TableCell className="tableCell" >{row.customer}</TableCell>
-              <TableCell className="tableCell" >{row.date}</TableCell>
-              <TableCell className="tableCell" >{row.amount}</TableCell>
-              <TableCell className="tableCell" >{row.method}</TableCell>
-              <TableCell className="tableCell" >{row.status}</TableCell>
+              <TableCell className="tableCell">{row.customer}</TableCell>
+              <TableCell className="tableCell">{row.date}</TableCell>
+              <TableCell className="tableCell">{row.amount}</TableCell>
+              <TableCell className="tableCell">{row.method}</TableCell>
+              <TableCell className="tableCell">
+                <span className={`status ${row.status}`}>{row.status}</span>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import "./single.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
 
 const Single = () => {
   return (
-    <div>
-      Single
+    <div className="single">
+      <Sidebar />
+      <div className="singleContainer">
+        <Navbar />
+        <div className="top">
+          <div className="left">
+           <div className="editButton">Edit</div>
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <img
+                src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+                alt="userProfile"
+                className="itemImg"
+              />
+              details
+            </div>
+          </div>
+          <div className="right"></div>
+        </div>
+        <div className="bottom"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Single
+export default Single;
